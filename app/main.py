8bac37ca1f3b04e99a34626e0fc5b6c7e9aed165
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-08 10:41:07 +0530

    feat: Add entrypoint

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from app.core.config import settings
+from app.api import api_router
+from app.middlewares.logging_middleware import LoggingMiddleware
+
+
+server = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
+
+server.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Logging Middleware
+server.add_middleware(LoggingMiddleware)
+
+# server.include_router(api_router, prefix="/api")
+
+@server.get("/")
+def index():
+    return {
+        "version": settings.VERSION
+    }
