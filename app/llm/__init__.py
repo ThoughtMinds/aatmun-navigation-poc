a0eb4b674a2fc5ac81fe0b3892acfa59a6d82bc9
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-09 12:38:20 +0530

    feat: Add base LLM class, structured output wrapper

---
+++ app/llm/__init__.py
@@ -0,0 +1,1 @@
+from .base import get_ollama_chat_model, get_ollama_chat_fallback_model, get_ollama_embeddings_model
--- app/llm/base.py
+++ app/llm/base.py
@@ -1,8 +1,23 @@
 from app.core.config import settings
 from langchain_ollama.embeddings import OllamaEmbeddings
 from langchain_ollama.chat_models import ChatOllama
+from app import schema
+from functools import wraps
+from langchain_core.language_models import BaseChatModel
 
 
+def with_navigation_output(func):
+    """Decorator to wrap a function that returns a ChatOllama model with structured Navigation output."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        chat_model: BaseChatModel = func(*args, **kwargs)
+        return chat_model.with_structured_output(schema.Navigation, method="json_schema")
+
+    return wrapper
+
+
+@with_navigation_output
 def get_ollama_chat_model():
     """Initialize an Ollama Chat Model for LLM inference
 
@@ -15,10 +30,25 @@ def get_ollama_chat_model():
     )
 
 
+@with_navigation_output
+def get_ollama_chat_fallback_model():
+    """Initialize a fallback Ollama Chat Model for LLM inference
+
+    Returns:
+        ChatOllama: Ollam Chat Model
+    """
+    return ChatOllama(
+        base_url=settings.OLLAMA_BASE_URL,
+        model=settings.OLLAMA_CHAT_FALLBACK_MODEL,
+    )
+
+
 def get_ollama_embeddings_model():
     """Initialize an Ollama Embeddings Model for LLM inference
 
     Returns:
         OllamaEmbeddings: Ollama Embeddings model
     """
-    return OllamaEmbeddings(base_url=settings.OLLAMA_BASE_URL, model=settings.OLLAMA_EMBEDDINGS_MODEL)
+    return OllamaEmbeddings(
+        base_url=settings.OLLAMA_BASE_URL, model=settings.OLLAMA_EMBEDDINGS_MODEL
+    )
