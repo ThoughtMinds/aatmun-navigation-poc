c2dc9f3910c8d40cccf7610aeb503dd7af85204c
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-09 12:39:35 +0530

    feat: Add project config vars

--- app/core/config.py
+++ app/core/config.py
@@ -7,13 +7,15 @@ class Settings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
-    OLLAMA_BASE_URL: str = None
-    OLLAMA_CHAT_MODEL: str = None
-    OLLAMA_EMBEDDINGS_MODEL: str = None
+    OLLAMA_BASE_URL: str
+    OLLAMA_CHAT_MODEL: str
+    OLLAMA_CHAT_FALLBACK_MODEL: str
+    OLLAMA_EMBEDDINGS_MODEL: str
+
+    CHROMA_PERSIST_DIRECTORY: str
 
     PROJECT_NAME: Optional[str] = "REST API"
     VERSION: Optional[str] = "v0.0.1"
-
 
 
 settings = Settings()
