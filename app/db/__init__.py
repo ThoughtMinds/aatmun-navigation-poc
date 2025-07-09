4aaf280a494906e66a5058954a2d14724d95dcc6
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-09 15:58:26 +0530

    feat: SqlModel models, dependency creation

---
+++ app/db/__init__.py
@@ -0,0 +1,1 @@
+from .models import Intent, Parameter, RequiredParameter, Response 
---
+++ app/db/models.py
@@ -0,0 +1,31 @@
+from sqlmodel import Field, SQLModel
+
+
+# SQLModel classes
+class Intent(SQLModel, table=True):
+    intent_id: int | None = Field(default=None, primary_key=True)
+    intent_name: str = Field(index=True, unique=True)
+    description: str | None = Field(default=None)
+
+class Parameter(SQLModel, table=True):
+    parameter_id: int | None = Field(default=None, primary_key=True)
+    intent_id: int = Field(foreign_key="intent.intent_id")
+    parameter_name: str = Field(index=True)
+    parameter_type: str
+    class Config:
+        table_args = ({"sqlite_autoincrement": True}, {"unique": ["intent_id", "parameter_name"]})
+
+class RequiredParameter(SQLModel, table=True):
+    required_id: int | None = Field(default=None, primary_key=True)
+    intent_id: int = Field(foreign_key="intent.intent_id")
+    parameter_name: str
+    class Config:
+        table_args = ({"sqlite_autoincrement": True}, {"unique": ["intent_id", "parameter_name"]})
+
+class Response(SQLModel, table=True):
+    response_id: int | None = Field(default=None, primary_key=True)
+    intent_id: int = Field(foreign_key="intent.intent_id")
+    platform: str
+    response_value: str
+    class Config:
+        table_args = ({"sqlite_autoincrement": True}, {"unique": ["intent_id", "platform"]})
