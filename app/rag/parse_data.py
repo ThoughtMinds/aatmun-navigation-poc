025f3cb182211f6240e487c64113714be25421b7
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-10 12:28:05 +0530

    refactor: Remove redundant BaseModel

--- app/rag/parse_data.py
+++ app/rag/parse_data.py
@@ -1,28 +1,15 @@
 from app import schema
-from typing import List, Dict, Optional
+from typing import List
 from langchain_core.documents import Document
 
 
-def parse_navigation_data(
-    navigation_data: List[Dict[str, str]],
-) -> Optional[List[schema.NavigationIntent]]:
-    navigation_intents: List[schema.NavigationIntent] = []
-
-    for item in navigation_data:
-        try:
-            intent = schema.NavigationIntent(**item)
-            navigation_intents.append(intent)
-        except Exception as e:
-            print(e)
-
-    return navigation_intents
-
-
-def get_documents(navigation_intents: List[schema.NavigationIntent]) -> List[Document]:
+def get_documents(navigation_intents: List[schema.IntentResponse]) -> List[Document]:
     documents: List[Document] = []
 
     for intent in navigation_intents:
-        doc = Document(page_content=intent.description, metadata={"id": intent.id})
+        doc = Document(
+            page_content=intent.description, metadata={"id": intent.intent_id}
+        )
         documents.append(doc)
 
     return documents
--- app/schema/__init__.py
+++ app/schema/__init__.py
@@ -1,3 +1,2 @@
-from .navigation_intent import NavigationIntent
 from .structured_output import Navigation
 from .database import IntentCreate, IntentResponse
