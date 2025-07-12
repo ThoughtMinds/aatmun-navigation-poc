25e1e21c9880affec4e2a15249b8b6c1ff6c4804
Abhinav <abhinav.km@thoughtminds.io>
2025-07-12 19:53:10 +0530

    feat: Change db init logic

--- app/rag/parse_data.py
+++ app/rag/parse_data.py
@@ -1,15 +1,24 @@
-from app import schema
-from typing import List
+from typing import Dict, List
 from langchain_core.documents import Document
+from app import schema
 
-
-def get_documents(navigation_intents: List[schema.IntentResponse]) -> List[Document]:
+def get_documents(navigation_intents: List[Dict]) -> List[Document]:
     documents: List[Document] = []
 
     for intent in navigation_intents:
-        doc = Document(
-            page_content=intent.description, metadata={"id": intent.intent_id}
-        )
-        documents.append(doc)
+        try:
+            doc = Document(page_content=intent["description"])
+            documents.append(doc)
+        except Exception as e:
+            print(f"Create document failed due to: {e}")
 
     return documents
+
+
+def get_documents(navigation_intent: schema.IntentCreate) -> Document:
+    documents: List[Document] = []
+    try:
+        doc = Document(page_content=navigation_intent.description)
+        documents.append(doc)
+    except Exception as e:
+        print(f"Create document failed due to: {e}")
