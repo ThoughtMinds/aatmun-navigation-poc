30e8823309a7a02bbc755699438a5bf3d21fb3ee
Abhinav <abhinav.km@thoughtminds.io>
2025-07-12 19:55:49 +0530

    feat: Create function to load data

---
+++ app/rag/load_data.py
@@ -0,0 +1,24 @@
+from json import load
+from typing import List, Dict
+from app.core.config import settings
+from os import path
+
+
+def load_sample_navigation_data() -> List[Dict]:
+    """Load sample navigation intents from a JSON file for database initialization purposes.
+
+    Returns:
+        List[Dict]: A list of dictionaries dictionary with navigation intents
+
+    """
+    if not path.isfile(settings.DATABASE_INIT_DATA):
+        return []
+
+    try:
+        with open(settings.DATABASE_INIT_DATA) as f:
+            sample_navigation_intents = load(f)
+    except Exception as e:
+        print(f"Failed to load navigation intents due to: {e}")
+        sample_navigation_intents = []
+        
+    return sample_navigation_intents
